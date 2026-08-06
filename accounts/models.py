153554fc9f16a6ed0dcfaa7de3e4bda3b3ffab8e
rasty22 <madanmaharjan073@gmail.com>
2026-08-06 10:10:44 +0545

    added login authentication

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
-from django.db import models  # type: ignore[import-not-found]
-from django.conf import settings  # type: ignore[import-not-found]
-from django.dispatch import receiver  # type: ignore[import-not-found]
-from django.db.models.signals import post_save  # type: ignore[import-not-found]
+from django.db import models  
+from django.conf import settings
+from django.dispatch import receiver 
+from django.db.models.signals import post_save 
 # Create your models here.
 
 class Profile(models.Model):
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from accounts.forms import SignUPForm
+from django.contrib.auth.forms import AuthenticationForm
 
 def signup_view(request):
     if request.method == "POST":
@@ -17,3 +18,21 @@ def signup_view(request):
         form = SignUPForm()
 
     return render(request, "accounts/signup.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":    #POST is use to CREATE or UPDATE data on the server.
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                next_url = request.POST.get('next') or request.GET('next')
+                return redirect(next_url or 'admin:index')
+        else:
+            form = AuthenticationForm
+
+        return render(request, 'admin:index', {'form': form})
+    
+    
