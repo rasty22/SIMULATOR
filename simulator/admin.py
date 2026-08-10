4676c72da6c499a3af0776575e21762f36e66d1f
rasty22 <madanmaharjan073@gmail.com>
2026-08-10 10:46:45 +0545

    changes in admin panels

--- simulator/admin.py
+++ simulator/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from simulator.models import Airport, Flight
 
-# Register your models here.
+
+@admin.register(Airport)
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ("name", "code", "city", "country")
+    search_fields = ("name", "code", "city", "country")
+
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = (
+        "flight_number",
+        "departure_airport",
+        "arrival_airport",
+        "departure_time",
+        "arrival_time",
+        "created_by",
+        "created_at",
+    )
+
+    search_fields = (
+        "flight_number",
+        "departure_airport__name",
+        "arrival_airport__name",
+    )
+
+    list_filter = (
+        "departure_airport",
+        "arrival_airport",
+        "departure_time",
+    )
